c08fadc26ede944152cf309a7fc8908ea2181a34
Shaun Persad <shaun.persad@envoy.com>
2021-06-03 20:55:00 -0400

    helper request types

--- src/EnvoyRequest.ts
+++ src/EnvoyRequest.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express';
 import EnvoyPluginSDK from './EnvoyPluginSDK';
-import EnvoyMeta from './EnvoyMeta';
+import EnvoyMeta, { EnvoyEventMeta, EnvoyRouteMeta } from './EnvoyMeta';
+import EntryPayload from './payloads/EntryPayload';
+import InvitePayload from './payloads/InvitePayload';
 
 export const VERIFIED = Symbol('verified');
 
@@ -11,3 +13,7 @@ export interface VerifiedRequest extends Request {
 export default interface EnvoyRequest <Meta extends EnvoyMeta = EnvoyMeta, Payload = any> extends VerifiedRequest {
   envoy: EnvoyPluginSDK<Meta, Payload>
 }
+
+export type EnvoyRouteRequest<Payload = any> = EnvoyRequest<EnvoyRouteMeta, Payload>;
+export type EnvoyEntryEventRequest = EnvoyRequest<EnvoyEventMeta, EntryPayload>;
+export type EnvoyInviteEventRequest = EnvoyRequest<EnvoyEventMeta, InvitePayload>;
