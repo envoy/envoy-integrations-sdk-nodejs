97c68c9e4a8a5363d8b03e84aee52c39c2507497
Shaun Persad <shaun.persad@envoy.com>
2021-06-04 15:25:26 -0400

    fixing docs

--- src/EnvoyResponse.ts
+++ src/EnvoyResponse.ts
@@ -5,6 +5,7 @@ import EnvoyPluginJobAttachment from './EnvoyPluginJobAttachment';
  * Use to type your `res` object in Envoy request handlers.
  */
 export default interface EnvoyResponse extends Response {
+  send: (debugInfo?: unknown) => this;
   sendOngoing: (debugInfo?: unknown) => void;
   sendIgnored: (message: string, debugInfo?: unknown, ...attachments: Array<EnvoyPluginJobAttachment>) => void;
   sendFailed: (message: string, debugInfo?: unknown, ...attachments: Array<EnvoyPluginJobAttachment>) => void;
