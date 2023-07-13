4a94868a77ee33161383f82c638a05f35a1ae86a
Ahmed Al-Hulaibi <ahmed@envoy.com>
2023-07-13 10:59:53 -0400

    fix: add sanitize axios err func

--- src/util/axiosConstructor.ts
+++ src/util/axiosConstructor.ts
@@ -5,29 +5,33 @@ export function createAxiosClient(config?: AxiosRequestConfig | undefined): Axio
     client.interceptors.response.use((response) => {
         return response;
     }, (error) => {
-        const safeError = {
-            code: error.code,
-            request: {
-                baseURL: error.request?.baseURL,
-                url: error.request?.url,
-                method: error.request?.method,
-            },
-            response: {
-                code: error.response?.code,
-                status: error.response?.status,
-                statusText: error.response?.statusText,
-                data: error.response?.data,
-            },
-            message: error.message,
-            name: error.name,
-            baseURL: error.request?.baseURL ?? error.config?.baseURL,
-            url: error.request?.url ?? error.config?.url,
-            method: error.request?.method ?? error.config?.method,
-            stack: error.stack,
-            data: error.data,
-        }
-        return Promise.reject(safeError);
+        return Promise.reject(sanitizeAxiosError(error));
     });
 
     return client;
+}
+
+export function sanitizeAxiosError(error: any) {
+    const safeError = {
+        code: error.code,
+        request: {
+            baseURL: error.request?.baseURL,
+            url: error.request?.url,
+            method: error.request?.method,
+        },
+        response: {
+            code: error.response?.code,
+            status: error.response?.status,
+            statusText: error.response?.statusText,
+            data: error.response?.data,
+        },
+        message: error.message,
+        name: error.name,
+        baseURL: error.request?.baseURL ?? error.config?.baseURL,
+        url: error.request?.url ?? error.config?.url,
+        method: error.request?.method ?? error.config?.method,
+        stack: error.stack,
+        data: error.data,
+    };
+    return safeError;
 }
