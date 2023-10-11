c85e5f23659e44a4eed8176daf49f6a64bbf8cd5
John Marrero <jmarrero@envoy.com>
2023-10-11 16:26:03 -0700

    FLAGINT-2727: add Kafka client

---
+++ src/sdk/KafkaClient.ts
@@ -0,0 +1,87 @@
+import { Kafka, KafkaConfig } from 'kafkajs';
+
+type ConsumeFunction = (message: string | undefined) => void;
+
+function buildConfig(clientId: string): KafkaConfig {
+  return {
+    clientId: clientId,
+    brokers: [
+      process.env.KAFKA_BROKER_1!,
+      process.env.KAFKA_BROKER_2!,
+      process.env.KAFKA_BROKER_3!
+    ],
+    ssl: true,
+    sasl: {
+      mechanism: 'scram-sha-512',
+      username: process.env.KAFKA_USERNAME!,
+      password: process.env.KAFKA_PASSWORD!,
+    },
+  };
+};
+
+class KafkaClient {
+  protected readonly config: KafkaConfig;
+  protected readonly topic: string;
+
+  constructor(clientId: string, topic: string) {
+    this.config = buildConfig(clientId);
+    this.topic = topic;
+  }
+
+  consumer(groupId: string, consumeFunction: ConsumeFunction) {
+    const kafka = new Kafka(this.config);
+    return new KafkaConsumer(kafka, this.topic, groupId, consumeFunction);
+  }
+
+  producer() {
+    const kafka = new Kafka(this.config);
+    return new KafkaProducer(kafka, this.topic);
+  }
+};
+
+class KafkaConsumer {
+  protected readonly kafka: Kafka;
+  protected readonly topic: string;
+  protected readonly groupId: string;
+  protected readonly consumeFunction: ConsumeFunction;
+
+  constructor(kafka: any, topic: string, groupId: string, consumeFunction: ConsumeFunction) {
+    this.kafka = kafka;
+    this.topic = topic;
+    this.groupId = groupId;
+    this.consumeFunction = consumeFunction;
+  }
+
+  async consume() {
+    const consumer = this.kafka.consumer({ groupId: this.groupId });
+    await consumer.connect();
+    await consumer.subscribe({ topic: this.topic });
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const messageValue = message.value?.toString();
+        await this.consumeFunction(messageValue);
+      },
+    });
+  }
+};
+
+class KafkaProducer {
+  protected readonly kafka: Kafka;
+  protected readonly topic: string;
+
+  constructor(kafka: any, topic: string) {
+    this.kafka = kafka;
+    this.topic = topic;
+  }
+
+  async produce(key: string, message: string) {
+    const producer = this.kafka.producer({ allowAutoTopicCreation: true });
+    await producer.connect();
+    await producer.send({
+      topic: this.topic,
+      messages: [{ key: key, value: message }],
+    });
+  }
+};
+
+module.exports = KafkaClient;
