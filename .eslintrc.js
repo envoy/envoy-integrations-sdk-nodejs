5b46efb866aa106b08f1c78f4a781fbe40133d6e
Justin Walters <jwalters@envoy.com>
2025-04-22 17:52:55 -0400

    Use prettier, disable eslint formatting rules
    We already have a prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,20 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
   rules: {
     'no-param-reassign': 'off',
     'no-void': 'off',
-    'max-len': ["error", { "code": 150 }],
+    'max-len': 'off',
     'consistent-return': 'off',
     'object-curly-newline': 'off',
     'import/prefer-default-export': 'off',
+    'function-paren-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    '@typescript-eslint/indent': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
@@ -22,21 +25,19 @@ module.exports = {
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/no-unused-expressions': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   overrides: [
     {
-      files: [
-        'test/**/*.ts'
-      ],
+      files: ['test/**/*.ts'],
       env: {
-        mocha: true
+        mocha: true,
       },
       rules: {
         'prefer-arrow-callback': 'off',
-        'func-names': 'off'
-      }
-    }
+        'func-names': 'off',
+      },
+    },
   ],
   ignorePatterns: ['*.js', '*.d.ts'],
 };
