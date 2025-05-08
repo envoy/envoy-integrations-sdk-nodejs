d8cba540f6bf9055a4b55e10a114ed7329c0ee6c
Justin Walters <jwalters@envoy.com>
2025-05-08 10:28:53 -0400

    Run typescript tests without legacy-peer-deps
    By downgrading jest and installing babel

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,19 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  verbose: true,
   preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 };
+
 export default config;
--- test/util/axios.test.ts
+++ test/util/axios.test.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosError } from 'axios';
 import { createAxiosClient } from '../../src/util/axiosConstructor';
 
 describe('axios', () => {
@@ -7,5 +8,60 @@ describe('axios', () => {
 
   it('should be defined', () => {
     expect(createAxiosClient).toBeDefined();
+  });
+
+  it('default client leaks credentials in AxiosError', async () => {
+    const client = axios.create({
+      headers: {
+        Authorization: 'Bearer 1234',
+      },
+      proxy: {
+        host: 'localhost',
+        port: 80,
+        auth: {
+          username: 'myWhackyUsername',
+          password: 'myWhackyPassword',
+        },
+      },
+    });
+    try {
+      await client.get('http://localhost:3000/axios-error');
+    } catch (error) {
+      expect(error.config).toBeDefined();
+      const errorStr = JSON.stringify(error);
+      expect(errorStr).toContain('Bearer 1234');
+      expect(errorStr).toContain('myWhackyUsername');
+      expect(errorStr).toContain('myWhackyPassword');
+    }
+  });
+
+  it('sanitizes AxiosError response', async () => {
+    const client = createAxiosClient({
+      headers: {
+        Authorization: 'Bearer 1234',
+      },
+      proxy: {
+        host: 'localhost',
+        port: 80,
+        auth: {
+          username: 'myWhackyUsername',
+          password: 'myWhackyPassword',
+        },
+      },
+    });
+    try {
+      await client.get('http://localhost:3000/axios-error');
+    } catch (error) {
+      expect(error).toBeInstanceOf(AxiosError);
+      expect(error.message).toBe('connect ECONNREFUSED 127.0.0.1:80');
+      expect(error.code).toBe('ECONNREFUSED');
+      expect(error.stack).toBeDefined();
+      expect(error.config.url).toBe('http://localhost:3000/axios-error');
+
+      const errorStr = JSON.stringify(error);
+      expect(errorStr).not.toContain('Bearer 1234');
+      expect(errorStr).not.toContain('myWhackyUsername');
+      expect(errorStr).not.toContain('myWhackyPassword');
+    }
   });
 });
