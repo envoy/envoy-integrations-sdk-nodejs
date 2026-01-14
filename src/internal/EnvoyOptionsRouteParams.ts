3dfb95e8a1ae3b4b8b671f83d7c8e2c9b749acf8
Justin Walters <jwalters@envoy.com>
2026-01-14 11:51:24 -0500

    Add parent_resource_id to OptionsRouteParam

--- src/internal/EnvoyOptionsRouteParams.ts
+++ src/internal/EnvoyOptionsRouteParams.ts
@@ -2,6 +2,10 @@ type EnvoyOptionsRouteParams = {
   search?: string;
   page?: number;
   cursor?: string;
+  zone_id?: string;
+  location_id?: string;
+  /* Id of selected option when this is a mapped field */
+  parent_resource_id?: string;
 };
 
 export default EnvoyOptionsRouteParams;
